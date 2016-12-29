efa04c93f78060600af24bb286159bf6ce5f8485
Stephen Lofgren <stephenlofgren@hotmail.com>
2016-12-29 15:56:18 -0700

    one more update to configuration file

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 venv --no-site-packages --distribute . && source ./bin/activate && pip install -r requirements.txt
+python3 -m venv --no-site-packages --distribute . && source ./bin/activate && pip install -r requirements.txt
 
